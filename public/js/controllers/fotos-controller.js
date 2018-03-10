61304980d924eaa540178b584376caf3bf662cb7
Edi Carlos note <edi_csilva@hotmail.com>
2018-03-10 19:08:23 -0300

    usado two way data binding com a diretiva ng-model para fazer o filtro no campo de pesquisa por titulo da imagem

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,10 +1,11 @@
 angular.module('alurapic').controller('FotosController', function($scope, $http) {
 
     $scope.fotos = [];
+    $scope.filtro = '';
 
     $http.get('v1/fotos')
         .success(function(retorno) {
-            console.log(retorno);
+            // console.log(retorno);
             $scope.fotos = retorno;
         })
         .error(function(erro) {
