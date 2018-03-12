d80f1f9f304b6331f002d58cf88befb5f019b21d
Edi Carlos note <edi_csilva@hotmail.com>
2018-03-12 11:44:24 -0300

    criado a primeira rota, com paginas parcias usando a diretiva ng-view. modulo ngRoute, usando rota padrão sem usar o hash # com html5Mode e dependencia

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -1,15 +1,31 @@
-angular.module('minhasDiretivas', []).directive('meuPainel', function() {
+angular.module('minhasDiretivas', [])
+    .directive('meuPainel', function() {
 
-    var ddo = {};
+        var ddo = {};
 
-    ddo.restrict = 'AE';
-    ddo.transclude = true;
+        ddo.restrict = 'AE';
+        ddo.transclude = true;
 
-    ddo.scope = {
-        titulo: '@'
-    }
+        ddo.scope = {
+            titulo: '@'
+        }
 
-    ddo.templateUrl = 'js/directives/meu-painel.html';
+        ddo.templateUrl = 'js/directives/meu-painel.html';
 
-    return ddo;
-});
+        return ddo;
+    })
+    .directive('minhaFoto', function() {
+
+        var ddo = {};
+
+        ddo.restrict = 'AE';
+
+        ddo.scope = {
+            url: '@',
+            titulo: '@'
+        }
+
+        ddo.template = '<img class="img-responsive center-block" ng-src="{{url}}" alt="{{titulo}}">';
+
+        return ddo;
+    });
