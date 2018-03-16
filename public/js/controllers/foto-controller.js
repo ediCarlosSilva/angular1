774878d778829e667d3395cc3c1f469cdb40d1e4
Edi Carlos note <edi_csilva@hotmail.com>
2018-03-16 09:35:42 -0300

    criado combobox usando ng-options do angular, usado filtro para uppercase

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -4,7 +4,7 @@ angular.module('alurapic').controller('FotoController', function($scope, $http, 
     $scope.mensagem = '';
 
     if ($routeParams.fotoId) {
-        $http.get('/v1/fotos/' + $routeParams.fotoId)
+        $http.get('v1/fotos/' + $routeParams.fotoId)
             .success(function(foto) {
                 $scope.foto = foto;
             })
@@ -16,19 +16,19 @@ angular.module('alurapic').controller('FotoController', function($scope, $http, 
     $scope.submeter = function() {
         console.log($scope.foto);
 
-        if ($scope.foto._id) {
-            $http.put('/v1/fotos/' + $scope.foto._id, $scope.foto)
+        if ($routeParams.fotoId) {
+            $http.put('v1/fotos/' + $scope.foto._id, $scope.foto)
                 .success(function() {
                     $scope.mensagem = 'Foto ' + $scope.foto.titulo + ' alterada com sucesso.';
                 })
                 .error(function(erro) {
                     console.log(erro);
-                    $scope.mensagem = 'Não foi possível atualizar a foto ' + $scope.foto.titulo;
+                    $scope.mensagem = 'Não foi possível alterar a foto ' + $scope.foto.titulo;
                 })
         }
 
         if ($scope.formulario.$valid) {
-            $http.post('/v1/fotos', $scope.foto)
+            $http.post('v1/fotos', $scope.foto)
                 .success(function() {
                     console.log('Foto adicionada com sucesso.');
                     $scope.foto = {};
