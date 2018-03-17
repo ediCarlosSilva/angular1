ee5d752cb8f6133a65f37b9e7a2138cba7d78b87
Edi Carlos note <edi_csilva@hotmail.com>
2018-03-17 10:22:15 -0300

    usado ngResource, criado primeiro servico

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,43 +1,73 @@
-angular.module('alurapic').controller('FotoController', function($scope, $http, $routeParams) {
+angular.module('alurapic').controller('FotoController', function($scope, $http, $routeParams, recursoFoto) {
 
     $scope.foto = {};
     $scope.mensagem = '';
 
     if ($routeParams.fotoId) {
-        $http.get('v1/fotos/' + $routeParams.fotoId)
-            .success(function(foto) {
-                $scope.foto = foto;
-            })
-            .error(function(erro) {
-                $scope.mensagem = 'Não foi possível obter a foto';
-            });
+
+        recursoFoto.get({ fotoId: $routeParams.fotoId }, function(foto) {
+            $scope.foto = foto;
+        }, function(erro) {
+            console.log(erro);
+            $scope.mensagem = 'Não foi possível obter a foto';
+        });
+
+        // $http.get('v1/fotos/' + $routeParams.fotoId)
+        //     .success(function(foto) {
+        //         $scope.foto = foto;
+        //     })
+        //     .error(function(erro) {
+        //         $scope.mensagem = 'Não foi possível obter a foto';
+        //     });
     }
 
     $scope.submeter = function() {
-        console.log($scope.foto);
-
-        if ($routeParams.fotoId) {
-            $http.put('v1/fotos/' + $scope.foto._id, $scope.foto)
-                .success(function() {
-                    $scope.mensagem = 'Foto ' + $scope.foto.titulo + ' alterada com sucesso.';
-                })
-                .error(function(erro) {
-                    console.log(erro);
-                    $scope.mensagem = 'Não foi possível alterar a foto ' + $scope.foto.titulo;
-                })
-        }
+        // console.log($scope.foto);
 
         if ($scope.formulario.$valid) {
-            $http.post('v1/fotos', $scope.foto)
-                .success(function() {
-                    console.log('Foto adicionada com sucesso.');
+
+            if ($routeParams.fotoId) {
+
+                recursoFoto.update({ fotoId: $scope.foto._id }, $scope.foto, function() {
+                    $scope.mensagem = 'Foto alterada com sucesso.';
+                }, function(erro) {
+                    console.log(erro);
+                    $scope.mensagem = 'Nâo foi possível alterar.';
+                })
+
+
+                // $http.put('v1/fotos/' + $scope.foto._id, $scope.foto)
+                //     .success(function() {
+                //         $scope.mensagem = 'Foto ' + $scope.foto.titulo + ' alterada com sucesso.';
+                //     })
+                //     .error(function(erro) {
+                //         console.log(erro);
+                //         $scope.mensagem = 'Não foi possível alterar a foto ' + $scope.foto.titulo;
+                //     });
+            } else {
+
+                recursoFoto.save($scope.foto, function() {
                     $scope.foto = {};
                     $scope.mensagem = 'Foto cadastrada com sucesso.';
-                })
-                .error(function(erro) {
-                    console.log('Não foi possível cadastrar a foto');
+                }, function(erro) {
+                    console.log(erro);
                     $scope.mensagem = 'Não foi possível cadastrar a foto.';
                 })
+
+
+                // $http.post('v1/fotos', $scope.foto)
+                //     .success(function() {
+
+                //         $scope.foto = {};
+                //         $scope.mensagem = 'Foto cadastrada com sucesso.';
+                //         $scope.formulario.$setPristine();
+                //     })
+                //     .error(function(erro) {
+                //         console.log('Não foi possível cadastrar a foto');
+                //         $scope.mensagem = 'Não foi possível cadastrar a foto.';
+                //     });
+            }
+
         }
     };
 });
